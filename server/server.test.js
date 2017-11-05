70bb91be44939c9076c198df8c64abb8bba56355
Andrew Bonner <andrewbonner92@gmail.com>
2017-11-05 16:28:27 -0500

    Add describe blocks.

--- server/server.test.js
+++ server/server.test.js
@@ -3,27 +3,33 @@ const expect = require('expect')
 
 var app = require('./server').app
 
-it('should return hello world response', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        error: "Page not found."
-      })
+describe('server', () => {
+  describe('GET /', () => {
+    it('should return hello world response', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            error: "Page not found."
+          })
+        })
+        .end(done)
     })
-    .end(done)
-})
+  })
 
-it('should return an array of users', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        name: 'Andrew',
-        age: 25
-      })
+  describe('GET /users', () => {
+    it('should return an array of users', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            name: 'Andrew',
+            age: 25
+          })
+        })
+        .end(done)
     })
-    .end(done)
+  })
 })
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,41 +2,54 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  var res = utils.add(33, 11)
+describe('utils', () => {
 
-  expect(res).toBe(44).toBeA('number')
-})
+  describe('#add', () => {
+    it('should add two numbers', () => {
+      var res = utils.add(33, 11)
 
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(4, 3, (sum) => {
-    expect(sum).toBe(7).toBeA('number')
-    done()
+      expect(res).toBe(44).toBeA('number')
+    })
   })
-})
 
-it('should square a number', () => {
-  var result = utils.square(7)
-
-  expect(result).toBe(49).toBeA('number')
-})
-
-it('should async square a number', (done) => {
-  utils.asyncSquare(7, (square) => {
-    expect(square).toBe(49).toBeA('number')
-    done()
+  describe('#asyncAdd', () => {
+    it('should async add two numbers', (done) => {
+      utils.asyncAdd(4, 3, (sum) => {
+        expect(sum).toBe(7).toBeA('number')
+        done()
+      })
+    })
   })
-})
 
-it('should verify first and last name are set', () => {
-  var user = {location: "Niles", age: 25}
-  var result = utils.setName(user, "Andrew Bonner")
+  describe('#square', () => {
+    it('should square a number', () => {
+      var result = utils.square(7)
 
-  expect(user).toNotEqual(result)
-  expect(result).toBeA('object')
-  expect(result).toInclude({
-    firstName: "Andrew",
-    lastName: "Bonner"
+      expect(result).toBe(49).toBeA('number')
+    })
+  })
+
+  describe('#asyncSquare', () => {
+    it('should async square a number', (done) => {
+      utils.asyncSquare(7, (square) => {
+        expect(square).toBe(49).toBeA('number')
+        done()
+      })
+    })
+  })
+
+  describe('#setName', () => {
+    it('should verify first and last name are set', () => {
+      var user = {location: "Niles", age: 25}
+      var result = utils.setName(user, "Andrew Bonner")
+
+      expect(user).toNotEqual(result)
+      expect(result).toBeA('object')
+      expect(result).toInclude({
+        firstName: "Andrew",
+        lastName: "Bonner"
+      })
+    })
   })
 })
 
